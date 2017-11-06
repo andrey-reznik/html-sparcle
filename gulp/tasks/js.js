128afbe8fef48e0ca0a38e2d73d9b258042eec57
Andrey Reznik <andrey.reznik@yahoo.com>
2017-11-06 16:39:15 +0300

    Added source-maps generator

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,18 +2,22 @@
 module.exports = function () {
     $.gulp.task('js:libs', function () {
         return $.gulp.src($.gp.requireReload($.config.gulpRoot + $.config.path.libs.js).src)
+            .pipe($.gp.if($.config.release, $.gp.sourcemaps.init()))        //Инициализация source-maps
             .pipe($.gp.concat('libs.js'))                                   //Конкатенация JS файлов
             .pipe($.gp.if($.config.release, $.gp.uglify()))                 //Минимизация JS файла
             .pipe($.gp.if($.config.release, $.gp.rename({suffix: '.min'}))) //Переименоввывание JS файла
+            .pipe($.gp.if($.config.release, $.gp.sourcemaps.write('/')))    //Запись source-map
             .pipe($.gulp.dest($.config.path.app.js))                        //Перемещение готового файла в папку JS
             .pipe($.gp.browserSync.reload({stream: true}));                 //Обновление браузера
     });
 
     $.gulp.task('js:app', function () {
         return $.gulp.src($.config.path.watch.js)
+            .pipe($.gp.if($.config.release, $.gp.sourcemaps.init()))        //Инициализация source-maps
             .pipe($.gp.concat('app.js'))                                    //Конкатенация JS файлов
             .pipe($.gp.if($.config.release, $.gp.uglify()))                 //Минимизация JS файла
             .pipe($.gp.if($.config.release, $.gp.rename({suffix: '.min'}))) //Переименоввывание JS файла
+            .pipe($.gp.if($.config.release, $.gp.sourcemaps.write('/')))    //Запись source-map
             .pipe($.gulp.dest($.config.path.app.js))                        //Перемещение готового файла в папку JS
             .pipe($.gp.browserSync.reload({stream: true}));                 //Обновление браузера
     });
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,11 +2,13 @@
 module.exports = function () {
      $.gulp.task('sass', function () {
         return $.gulp.src($.config.path.watch.style)
+            .pipe($.gp.if($.config.release, $.gp.sourcemaps.init()))            //Инициализация source-maps
             .pipe($.gp.sass().on('error', $.gp.notify.onError()))               //Сборка SASS в CSS + включение уведомлений в системном трее при ошибке
             .pipe($.gp.autoprefixer($.config.autoprefixer))                     //Добавление CSS свойствам префиксов для браузеров
             .pipe($.gp.groupCssMediaQueries())                                  //Группировка медиа запросов в CSS
             .pipe($.gp.if($.config.release, $.gp.cleanCss()))                   //Минификация CSS
-            .pipe($.gp.if($.config.release,  $.gp.rename({suffix: '.min'})))    //Переименовывание CSS файла
+            .pipe($.gp.if($.config.release, $.gp.rename({suffix: '.min'})))     //Переименовывание CSS файла
+            .pipe($.gp.if($.config.release, $.gp.sourcemaps.write('/')))        //Запись source-map
             .pipe($.gulp.dest($.config.path.app.css))                           //Перемещение CSS в папку для CSS
             .pipe($.gp.browserSync.reload({stream: true}));                     //Обновление браузера
     });
