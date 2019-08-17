7397381d6a51c022af4ae5beb7b7c2e5bdb0505a
Andrey Reznik <andrey.reznik@yahoo.com>
2019-08-17 19:06:38 +0300

    Добавлены библиотеки для SASS

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,7 +3,9 @@ module.exports = function () {
     $.gulp.task('sass', function () {
         return $.gulp.src($.config.path.watch.sass.src)
             .pipe($.gp.if($.config.release, $.gp.sourcemaps.init()))            //Инициализация source-maps
-            .pipe($.gp.sass().on('error', $.gp.notify.onError()))               //Сборка SASS в CSS + включение уведомлений в системном трее при ошибке
+            .pipe($.gp.sass({
+                    includePaths: ['node_modules/']
+                }).on('error', $.gp.notify.onError()))                          //Сборка SASS в CSS + включение уведомлений в системном трее при ошибке
             .pipe($.gp.autoprefixer($.config.autoPrefixer))                     //Добавление CSS свойствам префиксов для браузеров
             .pipe($.gp.groupCssMediaQueries())                                  //Группировка медиа запросов в CSS
             .pipe($.gp.if($.config.release, $.gp.cleanCss()))                   //Минификация CSS
